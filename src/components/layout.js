3e6552b88dd2ebea4dac93f587f5337e188887ff
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-06 23:40:43 +0100

    📱 Better mobile styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,7 @@ export default ({ children }) => (
           titleTemplate={`%s | ${data.site.siteMetadata.title}`}
           defaultTitle={data.site.siteMetadata.title}
         />
-        <div className="ma4 bg-near-white br2 min-vh-100 flex justify-center items-center work-sans f4">
+        <div className="ma4-ns ma3 bg-near-white br2 min-vh-100 flex justify-center items-center work-sans f4">
           {children}
         </div>
       </>
