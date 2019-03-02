49f0d3378f0e7d2a171d0632440856b844e5a065
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-02 20:33:04 +0100

    :wrench: Update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv-safe").config();
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -5,7 +7,36 @@ module.exports = {
     author: `@gatsbyjs`
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`
+      }
+    },
     `gatsby-transformer-sharp`,
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-trakt-tmdb`,
+      options: {
+        traktApiKey: process.env.TRAKT_API_KEY,
+        username: "wirtzdan",
+        tmdbApiKey: process.env.TMDB_API_KEY,
+        language: "en-US"
+      }
+    },
+    `gatsby-plugin-transition-link`,
+    {
+      resolve: `gatsby-source-spotify`,
+      options: {
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+        refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
+
+        fetchPlaylists: false,
+        fetchRecent: true,
+        timeRanges: ["short_term", "medium_term", "long_term"]
+      }
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
