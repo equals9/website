12e9e726a91f642afb6d1cd22b167f5e2b23b96e
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-02 20:37:27 +0100

    :wrench: Update  Component routing

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+import Footer from "./footer";
+import Header from "./header";
+import Layout from "./layout";
+import Section from "./section";
+import Container from "./container";
+
+export { Footer, Header, Layout, Section, Container };
