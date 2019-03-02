3aa0047480e475939f6199ab94e51917f16c6435
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-02 20:36:58 +0100

    :sparkles: Add footer component

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, Text } from "grommet";
+import { Container } from "../common/";
+
+const Footer = () => (
+  <Box background="dark-1" pad={{ vertical: "small" }}>
+    <Container>
+      <Text textAlign="center">Designed and build by Daniel Wirtz</Text>
+    </Container>
+  </Box>
+);
+
+export default Footer;
