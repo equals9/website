0fb2545d8c60a68649639b2f8d118d34c2c397c6
Daniel Wirtz <danielwirtzx@gmail.com>
2019-03-10 13:59:59 +0100

    :lipstick: Update header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,64 +1,90 @@
-import React from "react";
-import { Box, Text, FormField, CheckBox } from "grommet";
+import React, { useState } from "react";
+import { Box, Text, FormField, CheckBox, Layer } from "grommet";
 import { Section } from "./index";
 import Link from "./link";
 import styled from "styled-components";
 import Logo from "./logo";
 import ThemeContext from "../../util/context";
+import { Spin } from "react-burgers";
 
-const Header: React.SFC = () => (
-  <ThemeContext.Consumer>
-    {theme => (
-      <Section
-        pad={{ vertical: "medium" }}
-        background={theme.dark ? "dark-1" : "light-1"}
-      >
-        <Box direction="row" justify="between" align="center">
-          <Link to="/" label={theme.dark ? <Logo dark /> : <Logo />} />
-          <Box direction="row" gap="medium">
-            <StyledLink
-              to="/about"
-              color={theme.dark ? "light-1" : "dark-3"}
-              label={
-                <Text>
-                  <Prefix weight="normal" color="brand">
-                    01.
-                  </Prefix>{" "}
-                  About
-                </Text>
-              }
-            />
-            <StyledLink
-              to="/photography"
-              color={theme.dark ? "light-1" : "dark-3"}
-              label={
-                <Text>
-                  <Prefix weight="normal" color="brand">
-                    02.
-                  </Prefix>{" "}
-                  Photography
-                </Text>
-              }
-            />
+const Header: React.SFC = () => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <ThemeContext.Consumer>
+      {theme => (
+        <Section
+          pad={{ vertical: "medium" }}
+          background={theme.dark ? "dark-1" : "light-1"}
+        >
+          <Box direction="row" justify="between" align="center">
+            <Link to="/" label={theme.dark ? <Logo dark /> : <Logo />} />
+            <Box direction="row" gap="medium">
+              {/* <StyledLink
+                to="/about"
+                color={theme.dark ? "light-1" : "dark-3"}
+                label={
+                  <Text>
+                    <Prefix weight="normal" color="brand">
+                      01.
+                    </Prefix>{" "}
+                    About
+                  </Text>
+                }
+              />
+              <StyledLink
+                to="/photography"
+                color={theme.dark ? "light-1" : "dark-3"}
+                label={
+                  <Text>
+                    <Prefix weight="normal" color="brand">
+                      02.
+                    </Prefix>{" "}
+                    Photography
+                  </Text>
+                }
+              /> */}
+              <StyledSpin
+                color={theme.dark ? "#ffffff" : "#3B3B3B"}
+                borderRadius={5}
+                active={active}
+                onClick={() => setActive(!active)}
+              />
+              {active && (
+                <Layer full>
+                  <Box
+                    fill
+                    background="light-1"
+                    align="center"
+                    justify="center"
+                  />
+                </Layer>
+              )}
+            </Box>
           </Box>
-        </Box>
-      </Section>
-    )}
-  </ThemeContext.Consumer>
-);
+        </Section>
+      )}
+    </ThemeContext.Consumer>
+  );
+};
 
-const Prefix = styled(Text)`
-  text-transform: uppercase;
-  font-family: "Inconsolata", monospace;
+const StyledSpin = styled(Spin)`
+  z-index: 20;
+  position: relative;
 `;
 
-const StyledLink = styled(Link)`
-  text-decoration: none;
+// const Prefix = styled(Text)`
+//   text-transform: uppercase;
+//   font-family: "Inconsolata", monospace;
+// `;
 
-  &:hover {
-    color: #01d274;
-    text-decoration: none;
-  }
-`;
+// const StyledLink = styled(Link)`
+//   text-decoration: none;
+
+//   &:hover {
+//     color: #01d274;
+//     text-decoration: none;
+//   }
+// `;
 
 export default Header;
